f04adf85225e812d962f00a3ac5114c6540c730a
azchad-commits <chadmfrank@gmail.com>
2026-06-04 19:46:02 -0700

    Update urls.ts

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,2 +1,2 @@
 export const PRIVACY_POLICY_URL = 'https://azchad-commits.github.io/AgileIQ/privacy-policy.html';
-export const TERMS_OF_USE_URL = 'https://azchad-commits.github.io/AgileIQ/terms-of-use.html';
+export const TERMS_OF_USE_URL = 'https://www.apple.com/legal/internet-services/itunes/dev/stdeula/';
